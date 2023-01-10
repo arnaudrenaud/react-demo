cbd939de157c9158fa5898356b3c4b8ab3e67ff9
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-01-10 15:26:48 +0100

    Show names in table

--- src/NameGenerator/NameGenerator.js
+++ src/NameGenerator/NameGenerator.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NameGenerator = () => {
-  return <h1>Générateur de noms</h1>;
+  const [persons, setPersons] = useState([]);
+
+  const addNewPerson = async () => {
+    const response = await fetch("https://randomuser.me/api/");
+    const result = await response.json();
+
+    const newPerson = result.results[0];
+    setPersons(persons.concat([newPerson]));
+  };
+
+  return (
+    <>
+      <h1>Générateur de noms</h1>
+      <button onClick={addNewPerson}>Nouvelle personne</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Prénom</th>
+            <th>Nom</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person) => {
+            return (
+              <tr key={person.login.uuid}>
+                <td>{person.name.first}</td>
+                <td>{person.name.last}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default NameGenerator;
